890df70412f3454f98c14c52cbfda29ff969af67
max6296-admin <hjy6296@gmail.com>
2025-08-17 22:01:21 +0900

    20250817 복습 내용 (시험 공부)

--- REACT/my-react-app/src/Practice.tsx
+++ REACT/my-react-app/src/Practice.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import UserInfoContainer from './practice/1.PropsPractice';
 import BoardContainer from './practice/2.BoardContainer';
+import BoardContainer2 from './practice2/BoardContainer';
 import ModuleCssPractice from './practice/3.ModuleCssPrac';
 
 function Practice() {
@@ -11,7 +12,8 @@ function Practice() {
                     <h1 style={{ fontWeight: "bolder" }}>KH G CLASS</h1>
                     {/* <UserInfoContainer/> */}
                     {/* <BoardContainer/> */}
-                    <ModuleCssPractice/>
+                    {/* <ModuleCssPractice/> */}
+                    <BoardContainer2/>
                 </div>
             </div>
         </>
